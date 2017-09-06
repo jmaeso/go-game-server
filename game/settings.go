067bd15395f6cdbbf3e7d45b9028454b6d155815
jmaeso <jmaeso@enti.cat>
2017-09-06 15:33:45 +0200

    Add comments and fix typos

--- game/settings.go
+++ game/settings.go
@@ -1,10 +1,12 @@
 package game
 
+// ServerSettings are the yaml representation of the redis settings.
 type ServerSettings struct {
 	URL  string `yaml:"url"`
 	Port string `yaml:"port"`
 }
 
+// Settings is the model that stores all the different configuration models.
 type Settings struct {
 	Server *ServerSettings `yaml:"server"`
 }
